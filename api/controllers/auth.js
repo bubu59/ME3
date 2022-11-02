df509d1de86bcadc2a5378a6b3c20dc00c9139c8
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-11-02 19:13:45 +0800

    auth

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,8 +4,13 @@ import bcrypt from "bcryptjs"
 
 export const signin = async (req, res, next) => {
     try {
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt)
+        const newUser = new User({ ...req.body, password: hash })
 
-    } catch {
-
+        await newUser.save()
+        res.status(200).json("User has been created!")
+    } catch (err) {
+        next(err)
     }
 }
